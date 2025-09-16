0af903bddf627b87507735e90a7b3f142a55a5f5
Ryan-Fx <rinnd.dn@gmail.com>
2025-09-16 11:01:41 +0800

    Remove unused usePathname import from Header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import NavBar from "./nav-bar";
 import MobileNav from "./mobile-nav";
 
 export default function Header() {
-  const pathname = usePathname();
-
   return (
     <header className="sticky top-0 z-20">
       <NavBar />
